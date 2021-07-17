2e5df9c60214248c38be83abf5ca918d18ad7bee
Laura Parker <laura.parker03@yahoo.com>
2021-07-16 22:15:27 -0400

    Friday night commit

--- 02_ClaimsTest/ClaimsTest.cs
+++ 02_ClaimsTest/ClaimsTest.cs
@@ -23,17 +23,11 @@ namespace _02_ClaimsTest
         {
             int initialCount = _claimsRepo._queueOfClaims.Count;
             // Arrange --> Setting up
-            Claims newClaim = new Claims();
-            newClaim.ClaimID = 12345;
-            newClaim.ClaimType = "Car";
-            newClaim.Description = "Car in Accident on 465";
-            newClaim.ClaimAmount = 400.00m;
-            newClaim.DateOfIncident = new DateTime(01, 01, 2001);
-            newClaim.DateOfClaim = new DateTime(02, 02, 2001);
-            newClaim.IsValid = true;
+            Claims addClaim = new Claims(14444, "House", "House on Fire", 6000.00m, new DateTime(01 / 03 / 2003), new DateTime(04 / 31 / 2003), false);
+
 
             // Act --> Get/run the code needed to test
-            _claimsRepo.AddClaims(newClaim);
+            _claimsRepo.AddClaims(addClaim);
             int newCount = _claimsRepo._queueOfClaims.Count;
 
             // Assert --> Use the assert class to verify the expected outcome.
@@ -44,10 +38,11 @@ namespace _02_ClaimsTest
         public void DeleteClaimCountTest()
         {
             // Create Object    //Count
-            int initialCount = _claimsRepo._queueOfClaims.Count;
             Claims removeClaim = new Claims(14444, "House", "House on Fire", 6000.00m, new DateTime (01/03/2003), new DateTime (04/31/2003),false);
+            _claimsRepo.AddClaims(removeClaim);
+            int initialCount = _claimsRepo._queueOfClaims.Count;
 
-            _claimsRepo.DeleteClaimFromQueue(removeClaim);  // Dequeue a claim
+            _claimsRepo.DeleteClaimFromQueue();  // Dequeue a claim
             int newCount = _claimsRepo._queueOfClaims.Count; // Test Count
 
             // Compare
